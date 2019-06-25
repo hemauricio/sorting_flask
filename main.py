bfbe6764b1b61004bea15f8e9e4315a8d2864fe3
mmhernandez <mauriciohdez08@gmail.com>
2019-06-25 17:38:35 -0500

    Dummy Quicksort route added

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask("sorting_flask")
 
 @app.route("/")
 def hello():
     return "Hello, World!\n"
+
+@app.route("/quicksort", methods=["POST"])
+def quicksort():
+    return jsonify({
+        "data":{
+            "original":request.json,
+            "sorted":request.json # Temporal
+        }
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
